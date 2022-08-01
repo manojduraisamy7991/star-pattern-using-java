1e1918a1bd808473a66ded4f16eb6d39c6c46fad
Manoj-ReactJs <manojd@devsdrop.studio>
2022-08-01 18:23:40 +0530

    init commit

---
+++ src/main/java/com/example/Pattern/PatternApplication.java
@@ -0,0 +1,87 @@
+package com.example.Pattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatternApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatternApplication.class, args);
+//		pattern1(4);
+//		pattern2(4);
+//		pattern3(4);
+//		pattern4(4);
+//		pattern5(4);
+		pattern6(4);
+	}
+	static void pattern1(int n) {
+		for(int row = 1;row<=n;row++){
+			// foe every row, run the col
+			for (int col = 1 ; col <=n;col++){
+				System.out.print("* ");
+			}
+			// when one row is printed,we need to add a newline
+			System.out.println();
+
+		}
+	}
+	static void pattern2(int n) {
+		patternal(4);
+	}
+
+	static void patternal(int n) {
+		for(int row = 1;row<=n;row++){
+			// foe every row, run the col
+			for (int col = 1 ; col <=row;col++){
+				System.out.print("* ");
+			}
+			// when one row is printed,we need to add a newline
+			System.out.println();
+
+		}
+	}
+
+	static void pattern3(int n) {
+		for(int row = 1;row<=n;row++){
+			// foe every row, run the col
+			for (int col = 1 ; col <=n-row+1;col++){
+				System.out.print("* ");
+			}
+			// when one row is printed,we need to add a newline
+			System.out.println();
+
+		}
+	}
+	static void pattern4(int n) {
+		for(int row = 1;row<=n;row++){
+			// foe every row, run the col
+			for (int col = 1 ; col <=n;col++){
+				System.out.print("* ");
+			}
+			// when one row is printed,we need to add a newline
+			System.out.println();
+
+		}
+	}
+	static void pattern5(int n) {
+		for(int row = 1;row<=n;row++){
+			// foe every row, run the col
+			for (int col = 1 ; col <=row;col++){
+				System.out.print(col +" ");
+			}
+			// when one row is printed,we need to add a newline
+			System.out.println();
+		}
+	}
+
+	static void pattern6(int n){
+		for(int row=1;row<=n;row++){
+			for(int col=1;col<=n;col++){
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+	}
+
+}
